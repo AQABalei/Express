366b5d57c493f6ab7c4bd5802055da7b42840412
Zhong Chu <zhongchu@usc.edu>
2019-04-09 15:13:10 -0700

    unit tests

---
+++ __tests__/unit/track-test.js
@@ -0,0 +1,22 @@
+const expect = require("chai").expect;
+const assert = require("chai").assert;
+const Track = require("./../../models/track");
+
+describe("track", function(){
+  describe("milliseconds", function(){
+    it("should return error if millisecond is not a number.", async function(){
+      try {
+        let track = new Track({ milliseconds: "abc" });
+        await track.validate();
+      }
+      catch (error) {
+        expect(error.errors[0].message).to.equal("Milliseconds must be number.");
+      }
+    });
+
+    it("should pass if millisecond is a number.", async function(){
+        let track = new Track({milliseconds:10});
+        return expect.isNumber(track.milliseconds);
+    });
+  });
+});
